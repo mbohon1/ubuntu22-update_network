6f7738e533379197060fe5470baaed080fb479ad
mbohon1 <90018236+mbohon1@users.noreply.github.com>
2024-07-26 17:53:38 +0700

    Update update_network_config.sh

--- update_network_config.sh
+++ update_network_config.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Tìm địa chỉ IPv6 và gateway
+# Tìm địa chỉ IPv6
 IPV6ADDR=$(ip -6 addr show dev ens3 | grep 'inet6' | grep -v 'fe80' | awk '{print $2}' | cut -d/ -f1)
+
+# Tìm gateway IPv6
 IPV6GATEWAY=$(ip -6 route show default | awk '{print $3}')
 
 # Kiểm tra nếu không tìm thấy địa chỉ IPv6 hoặc gateway
@@ -11,8 +13,12 @@ if [ -z "$IPV6ADDR" ] || [ -z "$IPV6GATEWAY" ]; then
 fi
 
 NETPLAN_CONFIG="/etc/netplan/01-netcfg.yaml"
+BACKUP_CONFIG="/etc/netplan/01-netcfg.yaml.bak"
 
-# Tạo file cấu hình Netplan
+# Tạo bản sao lưu của file cấu hình Netplan hiện tại
+sudo cp $NETPLAN_CONFIG $BACKUP_CONFIG
+
+# Tạo file cấu hình Netplan mới
 cat <<EOT > $NETPLAN_CONFIG
 network:
   version: 2
@@ -30,8 +36,16 @@ network:
           - 2001:4860:4860::8844
 EOT
 
-# Áp dụng cấu hình Netplan
+# Áp dụng cấu hình Netplan mới
 sudo netplan apply
 
-# Kiểm tra kết nối IPv6
-ping6 google.com -c 4
+# Kiểm tra kết nối IPv6 trong 10 giây
+ping6 -c 4 google.com
+if [ $? -ne 0 ]; then
+  echo "Cấu hình mới không hoạt động. Khôi phục lại cấu hình gốc."
+  sudo cp $BACKUP_CONFIG $NETPLAN_CONFIG
+  sudo netplan apply
+  exit 1
+else
+  echo "Cấu hình mới hoạt động tốt."
+fi
